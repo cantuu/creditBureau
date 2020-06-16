1c9605eafc456a74c675c9fa34ce4a41ff453842
Gabriel Cantu <gab@certi.org.br>
2020-06-15 21:29:04 -0300

    feat: add lambda function codes

---
+++ lambdaFunctions/userData/index.js
@@ -0,0 +1,57 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+exports.handler = async (event) => {
+  const userCPF = event.queryStringParameters.cpf;
+
+  if (!userCPF) {
+    return response(400, { message: "Missing required CPF" });
+  }
+
+  const responseBody = await readFile(userCPF);
+  if (!responseBody) {
+    return response(404, { message: "CPF not found" });
+  }
+
+  return response(200, [responseBody]);
+};
+
+async function readFile(cpf) {
+  const filename = "user-data.json";
+  const bucket = {
+    Bucket: "creditdb",
+    Key: filename,
+    ResponseContentType: "application/json",
+  };
+
+  const data = await s3.getObject(bucket).promise();
+
+  return readFileContent(data.Body.toString(), cpf);
+}
+
+function readFileContent(content, cpf) {
+  return JSON.parse(content)[cpf];
+}
+
+function response(statusCode, body) {
+  let header = {};
+
+  if (statusCode === 200) {
+    header = {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    };
+  } else {
+    header = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    };
+  }
+
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(body),
+    headers: header,
+  };
+}
